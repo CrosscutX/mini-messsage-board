1926bbd04bf386d5c6b63f5cab14bea04ded368d
CrosscutX <crosscutcode@gmail.com>
2023-09-14 22:23:37 -0400

    Working message app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  const name = req.body.name;
+  const message = req.body.message;
+
+  messages.push({ text: message, user: name, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
